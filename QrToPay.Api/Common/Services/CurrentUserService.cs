32694dfd54cb33e28dc623114524d11a32f701c5
Fuchak <fufutrzak@vp.pl>
2024-09-23 15:37:58 +0200

    lil fix (i know i cant merge etc. but ths is faster)

--- QrToPay.Api/Common/Services/CurrentUserService.cs
+++ QrToPay.Api/Common/Services/CurrentUserService.cs
@@ -12,12 +12,4 @@ public class CurrentUserService
     public int UserId =>
        _httpContextAccessor.HttpContext?.Items["UserId"] as int?
        ?? throw new InvalidOperationException("UserId nie jest dostępny w kontekście HTTP.");
-
-    public string? Email =>
-        _httpContextAccessor.HttpContext?.Items["UserEmail"] as string
-        ?? throw new InvalidOperationException("Email nie jest dostępny w kontekście HTTP.");
-
-    public string? PhoneNumber =>
-        _httpContextAccessor.HttpContext?.Items["UserPhoneNumber"] as string
-        ?? throw new InvalidOperationException("Numer telefonu nie jest dostępny w kontekście HTTP.");
 }
